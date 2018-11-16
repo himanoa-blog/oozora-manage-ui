7564f9b8c6a6e4fe7317a0db3ee05070bbf61c46
himanoa <matsunoappy@gmail.com>
2018-11-17 03:05:24 +0900

    Add before login page

---
+++ src/components/pages/login.tsx
@@ -0,0 +1,9 @@
+import * as React from "react";
+
+export function Login() {
+  return (
+    <div className="flex justify-center">
+      <h1>遺言書</h1>
+    </div>
+  )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
+import { Login } from "./components/pages/login";
+
 const rootElement = document.getElementById("root");
 
 if (rootElement === undefined) {
   throw new Error("rootがありません");
 } else {
-  ReactDOM.render(<div>HELLO</div>, rootElement);
+  ReactDOM.render(<Login />, rootElement);
 }
