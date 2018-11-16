73e39f7f9b9f5a37ebbb8f221cd69bff850bb39f
himanoa <matsunoappy@gmail.com>
2018-11-17 00:41:26 +0900

    Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,49 @@
+const path = require("path");
+const ENV = process.env.NODE_ENV || "development";
+const DEV_PORT = process.env.PORT || 4444;
+const webpack = require("webpack");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = [
+  {
+    entry: {
+      app: ["./src/index.tsx"],
+      rssWorker: ["./src/workers/rssImportWorker.ts"]
+    },
+    output: {
+      filename: "[name].bundle.js",
+      chunkFilename: "[name].bundle.js",
+      path: __dirname + "/dist",
+      publicPath: "/"
+    },
+    devServer: {
+      contentBase: "dist/",
+      historyApiFallback: true,
+      port: DEV_PORT
+    },
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", "jsx"]
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader"
+        },
+        { test: /\.(ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" }
+      ]
+    },
+    devtool: process.env.NODE_ENV === "production" ? "" : "inline-source-map",
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+        API_HOST: process.env.PROD_APIHOST
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        sourceMap: process.env.NODE_ENV === "production" ? false : true
+      })
+    ]
+  }
+];
