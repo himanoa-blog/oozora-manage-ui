b295ed562e97496e3a378811382214afe37d4eec
himanoa <matsunoappy@gmail.com>
2018-11-17 01:15:19 +0900

    Fix test match regex

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,5 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  testMatch: ["**/tests/**/*.js?(x),**/?(*.)+(spec|test).js?(x)","**/tests/**/*.ts?(x),**/?(*.)+(spec|test).ts?(x)"]
 };
