4e477ccd2cb4df6c30505183430e4f8df40ffac5
himanoa <matsunoappy@gmail.com>
2018-11-17 01:01:00 +0900

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -2,8 +2,10 @@ const path = require("path");
 const ENV = process.env.NODE_ENV || "development";
 const DEV_PORT = process.env.PORT || 4444;
 const webpack = require("webpack");
+const { CheckerPlugin } = require('awesome-typescript-loader')
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin")
 
-module.exports = [
+module.exports = (env, argv) => (
   {
     entry: {
       app: ["./src/index.tsx"]
@@ -26,17 +28,27 @@ module.exports = [
       rules: [
         {
           test: /\.tsx?$/,
-          use: "ts-loader"
+          use: "awesome-typescript-loader"
         },
         { test: /\.(ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/, loader: "file-loader" }
       ]
     },
-    devtool: process.env.NODE_ENV === "production" ? "" : "inline-source-map",
+    devtool: argv.mode === "production" ? "" : "inline-source-map",
+    optimization: {
+      minimizer: [
+        new UglifyJsPlugin({
+          cache: true,
+          parallel: true,
+          sourceMap: false // set to true if you want JS source maps
+        }),
+      ]
+    },
     plugins: [
+      new CheckerPlugin(),
       new webpack.DefinePlugin({
         "process.env.NODE_ENV": JSON.stringify("production"),
         API_HOST: process.env.PROD_APIHOST
       })
     ]
   }
-];
+)
