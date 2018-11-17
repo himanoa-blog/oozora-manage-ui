1a42b3cbfad48d3c861e4e6235728272ecee56f3
himanoa <matsunoappy@gmail.com>
2018-11-17 15:34:57 +0900

    Implement login page

---
+++ src/components/atoms/SigninButton.tsx
@@ -0,0 +1,14 @@
+import * as React from "react";
+
+interface Props{
+  onClick: (e) => void;
+  text: string;
+  className: string,
+}
+export function SigninButton(props: Props) {
+  return (
+    <button onClick={props.onClick} className={`${props.className} bg-blue-dark text-grey-light font-bold py-2 px-4 rounded inline-flex items-center`}>
+      {props.text}
+    </button>
+  )
+}
--- src/components/pages/login.tsx
+++ src/components/pages/login.tsx
@@ -1,9 +1,17 @@
 import * as React from "react";
+import { SigninButton } from "../atoms/SigninButton";
+
+function onClick(e: Event): void {
+  console.log("clicked!")
+}
 
 export function Login() {
   return (
-    <div className="flex justify-center">
-      <h1>遺言書</h1>
+    <div className="h-screen w-full flex flex-col items-center justify-center login-page">
+      <div className="z-10 max-w-xs max-h-full text-center">
+        <h1 className="text-grey-light">遺言書管理局</h1>
+        <SigninButton onClick={onClick} text="Sign in with Google" className="mt-4" />
+      </div>
     </div>
   )
 }
